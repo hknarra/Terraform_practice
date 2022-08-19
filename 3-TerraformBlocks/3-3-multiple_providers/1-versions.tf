9edf31ac5f3a4be9f43df805382dc03f48f5cc03
hknarra <hk.narra@gmail.com>
2022-08-19 11:45:54 +0530

    c3

---
+++ 3-TerraformBlocks/3-3-multiple_providers/1-versions.tf
@@ -0,0 +1,25 @@
+# Terraform Block
+terraform {
+  required_version = "~>1.2.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.26.0"
+  }
+  }
+}
+
+# Provider block
+# Provider 1 (default provider)
+provider "aws" {
+    region = "us-east-1"
+    profile = "default"  # shared credential file (authentication) 
+}
+
+# Provider 2
+provider "aws" {
+    region = "us-west-1"
+    profile = "default"
+    alias = "aws-west-1"  # Give alias for non default provider
+  
+}
