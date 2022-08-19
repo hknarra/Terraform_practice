2c4788150684ae0107a7efadcb252f547d4bac56
hknarra <hk.narra@gmail.com>
2022-08-19 21:35:07 +0530

    c3

---
+++ 4-Terraform-Resources/4-1-Resource-Syntax-and-Behavior/2-ec2-insatnce.tf
@@ -0,0 +1,13 @@
+# Create EC2 Instance
+# https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/instance
+
+resource "aws_instance" "my-ec2-vm" {
+  ami               = "ami-090fa75af13c156b4"
+  instance_type     = "t2.micro"
+  availability_zone = "us-east-1a"
+  #availability_zone = "us-east-1b"
+  tags = {
+    "Name" = "web"
+    #"tag1" = "Update-test-1"    
+  }
+}
