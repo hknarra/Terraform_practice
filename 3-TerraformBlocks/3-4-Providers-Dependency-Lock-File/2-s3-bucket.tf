32a7fba2076da346170efd1b2c0ba6fa916c04f0
hknarra <hk.narra@gmail.com>
2022-08-19 13:32:17 +0530

    c1

---
+++ 3-TerraformBlocks/3-4-Providers-Dependency-Lock-File/2-s3-bucket.tf
@@ -0,0 +1,11 @@
+# Resource Block: Create Random Pet Name 
+resource "random_pet" "petname" {
+  length    = 5
+  separator = "-"
+}
+
+# Resource Block: Create AWS S3 Bucket
+resource "aws_s3_bucket" "sample" {
+  bucket = random_pet.petname.id
+  acl    = "public-read"
+}
