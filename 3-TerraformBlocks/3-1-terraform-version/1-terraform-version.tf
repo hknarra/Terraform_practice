ea65eadd2231647c6ee17ceef7a517a0345bf9b9
hknarra <hk.narra@gmail.com>
2022-08-19 09:54:34 +0530

    c5

--- 3-TerraformBlocks/3-1-terraform-version/1-terraform-version.tf
+++ 3-TerraformBlocks/3-1-terraform-version/1-terraform-version.tf
@@ -1,15 +1,12 @@
 # Terraform Block
 terraform {
-  required_version = "1.2.0"
+  required_version = "~>1.2.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.26.0"
   }
-  name = {
-    source = "value"
-   }
-}
+  }
 }
 
 # Provider block
