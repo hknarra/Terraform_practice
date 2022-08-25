b35541cc499bf81fda78f89a3c3a1fca56507562
hknarra <hk.narra@gmail.com>
2022-08-25 16:18:00 +0530

    c5

--- 07-Terraform-State/07-01-Terraform-Remote-State-Storage-and-Locking/terraform-manifests/c1-versions.tf
+++ 07-Terraform-State/07-01-Terraform-Remote-State-Storage-and-Locking/terraform-manifests/c1-versions.tf
@@ -1,21 +1,21 @@
 # Terraform Block
 terraform {
-  required_version = "~> 1.2.0"  # which means any version equal & above 1.2.0 like 1.2.1, 1.2.2 etc and < 1.3.0
+  required_version = "~> 1.2.0" # which means any version equal & above 1.2.0 like 1.2.1, 1.2.2 etc and < 1.3.0
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 4.26.0"
-  }
+    }
   }
   # Adding Backend as S3 for Remote State Storage
   backend "s3" {
     bucket = "dev-hk-terraform-bucket"
     key    = "dev/terraform.tfstate"
-    region = "us-east-1" 
+    region = "us-east-1"
 
     # Enable during Step-09     
     # For State Locking
-    dynamodb_table = "terraform-dev-state-table"    
+    # dynamodb_table = "terraform-dev-state-table"    
 
   }
 }
