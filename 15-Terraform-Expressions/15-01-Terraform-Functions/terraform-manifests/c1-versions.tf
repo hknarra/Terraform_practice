9ec18d95ca379436b714660c2733b3c92e146139
hknarra <hk.narra@gmail.com>
2022-09-05 13:23:51 +0530

    c7

---
+++ 15-Terraform-Expressions/15-01-Terraform-Functions/terraform-manifests/c1-versions.tf
@@ -0,0 +1,18 @@
+# Terraform Block
+terraform {
+  required_version = "~> 1.2.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.26.0"
+  }
+  }
+}
+
+# Provider Block
+provider "aws" {
+  region  = var.aws_region
+  profile = "default"
+}
+# Note:  AWS Credentials Profile (profile = "default") configured on local desktop terminal  
+# check this is in terminal: $HOME/.aws/credentials
