2f77720b54eedb92bf1c421c5cff34a8c012fde5
hknarra <hk.narra@gmail.com>
2022-09-07 11:44:15 +0530

    c3

--- 04-Terraform-Resources/4-2-MetaArguemnet-Dependson/4-elasticip.tf
+++ 04-Terraform-Resources/4-2-MetaArguemnet-Dependson/4-elasticip.tf
@@ -1,7 +1,7 @@
 # Resource-9: Create Elastic IP
 resource "aws_eip" "my-eip" {
   instance = aws_instance.my-ec2-vm.id
-  vpc      = true
+  vpc      = true # EIP is in vpc or not
   # Meta-Argument
   depends_on = [aws_internet_gateway.vpc-dev-igw]
 }
