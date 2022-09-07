99c6a4ba82a3bbd533e330f67dff54256478136f
hknarra <hk.narra@gmail.com>
2022-09-07 17:05:20 +0530

    c3

--- 05-Terraform-Variables/05-01-Terraform-Input-Variables/v10-Sensitive-Input-Variables/c3-rds-db.tf
+++ 05-Terraform-Variables/05-01-Terraform-Input-Variables/v10-Sensitive-Input-Variables/c3-rds-db.tf
@@ -4,7 +4,7 @@ resource "aws_db_instance" "db1" {
   allocated_storage   = 5
   engine              = "mysql"
   instance_class      = "db.t2.micro"
-  name                = "mydb1"
+  db_name             = "mydb1"
   username            = var.db_username
   password            = var.db_password
   skip_final_snapshot = true
